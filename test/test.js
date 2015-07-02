d0b880e72d9ac338e748b100aed7170bb08fd11f
Joseph Dykstra <josephdykstra@gmail.com>
2015-07-02 07:55:04 -0500

    fixed tests so they're cross-platform

--- test/test.js
+++ test/test.js
@@ -37,37 +37,28 @@ test('extension', function (t) {
 	t.end()
 })
 
-test('error handling sync', function (t) {
-	var ws = createTempFile()
-	ws.on('error', function (e) {
-		t.equal(e.code, 'EPERM', 'Got EPERM error')
-		t.end()
-	})
-	setTimeout(function () {
-		try {
-			fs.unlinkSync(ws.path)
-		} catch (e) {
-			t.fail(String(e))
-		}
-		setTimeout(ws.cleanupSync, 10)
-	}, 10)
-})
+function errorHandling(method) {
+	var codes = [ 'EPERM', 'ENOENT' ]
+	return function errhandle(t) {
+		var ws = createTempFile()
+		ws.on('error', function (e) {
+			t.notEqual(codes.indexOf(e.code), -1, 'Got ' + codes.join('/') + ' error')
+			t.end()
+		})
+		setTimeout(function () {
+			try {
+				fs.unlinkSync(ws.path)
+			} catch (e) {
+				t.fail(String(e))
+			}
+			setTimeout(ws[method], 10)
+		}, 10)
+	}
+}
 
-test('error handling async', function (t) {
-	var ws = createTempFile()
-	ws.on('error', function (e) {
-		t.equal(e.code, 'EPERM', 'Got EPERM error')
-		t.end()
-	})
-	setTimeout(function () {
-		try {
-			fs.unlinkSync(ws.path)
-		} catch (err) {
-			t.fail(String(err))
-		}
-		setTimeout(ws.cleanup, 10)
-	}, 10)
-})
+test('error handling sync', errorHandling('cleanupSync'))
+
+test('error handling async', errorHandling('cleanup'))
 
 test('cleanupSync() works', function (t) {
 	t.plan(3)
